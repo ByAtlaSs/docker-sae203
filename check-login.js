0ebf6ed24a0ace1b0c4c8c1cabf9f94ead89fc07
ByAtlaSs <166228818+ByAtlaSs@users.noreply.github.com>
2024-04-16 06:09:07 +0200

    Delete check-login.js

--- check-login.js
+++
@@ -1,7 +0,0 @@
-// Vérifier si l'utilisateur est déjà connecté
-const storedUsername = localStorage.getItem('username');
-
-// Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
-if (!storedUsername) {
-    window.location.href = 'login.html';
-}
